bef11cb9de8bfdf4ff212cd5dac82dba826f9453
Rafal Oldziej <rafal.oldziej1@gmail.com>
2022-06-16 19:30:37 -0400

    commit tree 39908ff36f90596678ae9e3ebbd880b40848d6fa

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
 console.log('%c HI', 'color: firebrick')
+
+
+function addDog() {
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+    fetch(imgUrl)
+        .then(res => res.json())
+        .then(json => renderImage(json));
+
+}
+
+function renderImage(images) {
+    const loc = document.querySelector('div')
+  images.message.forEach(image => {
+    const img = document.createElement('img');
+    img.src = image;
+    loc.appendChild(img)
+  });
+}
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    addDog()
+  })
